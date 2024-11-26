12ca6cac43169175a38fb82f675ab474242cefe9
Aleksandra Melnikova <Aleksa1992.14.11@mail.ru>
2024-11-26 16:29:30 +0600

    Refactor code

--- 02and01/index.js
+++ 02and01/index.js
@@ -5,19 +5,16 @@ const port = 8000;
 const Vigenere = require('caesar-salad').Vigenere;
 const password = `Hello`;
 
-// Vigenere.Cipher('password').crypt('abc-0123456789@example.com');
-// Vigenere.Decipher('password').crypt('pbu-0123456789@wtodsae.ugi');
-
 app.get('/', (req, res) => {
     return res.send('Home page');
-})
+});
 
 app.get('/:response', (req, res) => {
     return res.send(`${req.params.response}`);
 });
 
 app.get(`/encode/:password`, (req, res) => {
-    return res.send(Vigenere.Cipher(`${password}`).crypt(`${req.params.password}`))
+    return res.send(Vigenere.Cipher(`${password}`).crypt(`${req.params.password}`));
 });
 
 app.get(`/decode/:password`, (req, res) => {
